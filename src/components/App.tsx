b4bab8f67b2bd7c7f57911d6ac99290f4b26d80c
Vlad Piatyhor <vlad.piatyhor@gmail.com>
2022-11-11 17:57:03 -0500

    add firebase init

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { config } from '../config';
+
+initializeApp(config.firebaseConfig);
 
 const App = (): JSX.Element => {
   return (
