fb18f1531fc5d76bc447c3792801e807076bf7f3
Vlad Piatyhor <vlad.piatyhor@gmail.com>
2022-11-12 14:40:28 -0500

    add Profile page logic, layout ans styles

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { User } from 'firebase/auth';
+import { auth } from "./../../firebase";
+import { Alert, Avatar, Button, Grid } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { styles } from './styles';
 
-export const Profile = (): JSX.Element => {
+export const Profile = (): JSX.Element => {  
+  const [error, setError] = useState('');
+
+  const user: User = auth.currentUser!;
+  const {email, photoURL} = user;
+
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    if (error) {
+      setError('');
+    }
+    
+    try {
+      await auth.signOut();
+      navigate('/login');
+    } catch (error) {
+      setError(`Error occur during log out: ${error}`);
+    }
+  }
+
   return (
-    <div>
-      Profile
-    </div>
+    <Grid
+      style={styles.root} 
+      container 
+      direction="column" 
+      justifyContent="center" 
+      alignItems="center"
+    >
+      <h2 style={styles.title}>User profile</h2>
+      {error && <Alert severity="error">{error}</Alert>}
+      <Avatar src={photoURL || ''} alt='User avatar' style={styles.avatar} />
+      <div>Email: <strong>{email}</strong></div>
+      <Button 
+        style={styles.logOutBtn} 
+        variant="outlined"
+        onClick={handleLogOut}
+      >
+        Log Out
+      </Button>
+    </Grid>
   );
 }
--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -1,1 +1,16 @@
-export const styles = {};
+export const styles = {
+    root: {
+        height: "100vh",
+    },
+    title: {
+        margin: "30px",
+    },
+    avatar: {
+        height: "125px",
+        width: "125px",
+        marginBottom: "20px",
+    },
+    logOutBtn: {
+        marginTop: "50px",
+    },
+};
